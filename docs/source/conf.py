8dc18ca1ed25cf0fbb542a055685e73c4f8d653c
xuzizheng <wan19950504@gmail.com>
2024-11-28 20:12:56 +0800

    ✨ feat(conf.py):
    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,40 @@ extensions = [
     "sphinx.ext.todo",
 ]
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.8", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+    "pst": ("https://pydata-sphinx-theme.readthedocs.io/en/latest/", None),
+}
+nitpick_ignore = [
+    ("py:class", "docutils.nodes.document"),
+    ("py:class", "docutils.parsers.rst.directives.body.Sidebar"),
+]
+
+suppress_warnings = ["myst.domains", "ref.ref"]
+
+numfig = True
+
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    # "html_admonition",
+    # "html_image",
+    "colon_fence",
+    # "smartquotes",
+    # "replacements",
+    # "linkify",
+    # "substitution",
+]
 
 language = 'zh-cn'
 
@@ -45,9 +77,119 @@ language = 'zh-cn'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
+html_logo = "_static/logo-wide.svg"
+html_title = "Sphinx Book Theme"
+html_copy_source = True
+html_favicon = "_static/logo-square.svg"
+html_last_updated_fmt = ""
 html_static_path = ['_static']
 
+html_sidebars = {
+    "reference/blog/*": [
+        "navbar-logo.html",
+        "search-field.html",
+        "ablog/postcard.html",
+        "ablog/recentposts.html",
+        "ablog/tagcloud.html",
+        "ablog/categories.html",
+        "ablog/archives.html",
+        "sbt-sidebar-nav.html",
+    ]
+}
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+nb_execution_mode = "cache"
+thebe_config = {
+    "repository_url": "https://github.com/Abner1995/snotes.git",
+    "repository_branch": "main",
+}
+
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/Abner1995/snotes.git",
+    "repository_branch": "main",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com/",
+        "deepnote_url": "https://deepnote.com/",
+        "notebook_interface": "jupyterlab",
+        "thebe": True,
+        # "jupyterhub_url": "https://datahub.berkeley.edu",  # For testing
+    },
+    "use_edit_page_button": True,
+    "use_source_button": True,
+    "use_issues_button": True,
+    # "use_repository_button": True,
+    "use_download_button": True,
+    "use_sidenotes": True,
+    "show_toc_level": 2,
+    "announcement": (
+        # "⚠️The latest release refactored our HTML, "
+        # "so double-check your custom CSS rules!⚠️"
+    ),
+    "logo": {
+        "image_dark": "_static/logo-wide-dark.svg",
+        # "text": html_title,  # Uncomment to try text with logo
+    },
+    "icon_links": [
+        {
+            "name": "Executable Books",
+            "url": "https://executablebooks.org/",
+            "icon": "_static/ebp-logo.png",
+            "type": "local",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/executablebooks/sphinx-book-theme",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/sphinx-book-theme/",
+            "icon": "https://img.shields.io/pypi/dw/sphinx-book-theme",
+            "type": "url",
+        },
+    ],
+    # For testing
+    # "use_fullscreen_button": False,
+    # "home_page_in_toc": True,
+    # "extra_footer": "<a href='https://google.com'>Test</a>",  # DEPRECATED KEY
+    # "show_navbar_depth": 2,
+    # Testing layout areas
+    # "navbar_start": ["test.html"],
+    # "navbar_center": ["test.html"],
+    # "navbar_end": ["test.html"],
+    # "navbar_persistent": ["test.html"],
+    # "footer_start": ["test.html"],
+    # "footer_end": ["test.html"]
+}
+
+# sphinxext.opengraph
+ogp_social_cards = {
+    "image": "_static/logo-square.png",
+}
+
+# -- sphinx-copybutton config -------------------------------------
+copybutton_exclude = ".linenos, .gp"
 
 # -- ABlog config -------------------------------------------------
-
+blog_path = "reference/blog"
+blog_post_pattern = "reference/blog/*.md"
+blog_baseurl = "https://snotes.readthedocs.io"
+fontawesome_included = True
+post_auto_image = 1
+post_auto_excerpt = 2
+nb_execution_show_tb = "READTHEDOCS" in os.environ
 bibtex_bibfiles = ["references.bib"]
+# To test that style looks good with common bibtex config
+bibtex_reference_style = "author_year"
+bibtex_default_style = "plain"
+numpydoc_show_class_members = False  # for automodule:: urllib.parse stub file issue
+linkcheck_ignore = [
+    "http://someurl/release",  # This is a fake link
+    "https://doi.org",  # These don't resolve properly and cause SSL issues
+]
+linkcheck_exclude_documents = ["changelog"]
