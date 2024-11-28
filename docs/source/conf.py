fb188e207cb1891ae2e12eb127cb9d4bd092c0e8
xuzizheng <wan19950504@gmail.com>
2024-11-28 19:36:47 +0800

    ✨ feat(extensions):
    extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,9 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import os
+from urllib.request import urlopen
+from pathlib import Path
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +16,25 @@ release = '1.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "ablog",
+    "myst_nb",
+    "numpydoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.youtube",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_examples",
+    "sphinx_tabs.tabs",
+    "sphinx_thebe",
+    "sphinx_togglebutton",
+    "sphinxcontrib.bibtex",
+    "runthis.sphinxext",
+    # For the kitchen sink
+    "sphinx.ext.todo",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
